0afbc5b4915272f19bd898889f371b397dbd2514
Dolu <Dolu89@users.noreply.github.com>
2023-02-13 11:57:58 +0100

    chore: new project structure (#8)
    * chore: new project structure
    
    * docs: add README.md

---
+++ start/routes.ts
@@ -0,0 +1,12 @@
+import Route from '@ioc:Adonis/Core/Route'
+import Env from "@ioc:Adonis/Core/Env"
+import NostrSocket from '../app/Services/NostrSocket'
+
+Route.get('/', async ({ view }) => {
+  if (!NostrSocket.booted) return { message: 'Nostr Proxy is booting...' }
+
+  const proxyUrl = Env.get('PROXY_URL')
+  const relays = await NostrSocket.getRelays()
+  const relaysCount = relays.length
+  return view.render('welcome', { proxyUrl, relays, relaysCount })
+})
